1610cdde6c95cde80e8f2b364fbc1c62e88419f3
dynch1k3 <93707944+dynch1k3@users.noreply.github.com>
2022-12-09 17:50:41 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# from random import *
+# x = randint(0, 10)
+# y = randint(0, 10)
+#
+# while ((x > 0) and (x <= 10) and (y > 0) and (y <= 10)):
+#     x = randint(0, 10)
+#     y = randint(0, 10)
+#     print("в зоне действия сети")
+#
+# else:
+#     print("вне зоны действия сети")
+# print(x,y)
+mass = [112, 2, 3, 4, 5, 6, 7, 8, 1, 9, 10, 11, 123]
+for i in range(0, 11):
+    mass.insert(i,i)
+     print(mass[1])
